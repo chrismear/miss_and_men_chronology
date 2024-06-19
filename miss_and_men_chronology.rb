2fcd3d141c3e5659b7778bb2e33f6b05afe4b50c
Chris Tucker Mear <chris@feedmechocolate.com>
2024-06-19 20:44:13 +0100

    Add more books

--- miss_and_men_chronology.rb
+++ miss_and_men_chronology.rb
@@ -6,10 +6,33 @@ require_relative 'lib/attribute'
 require_relative 'lib/ordering'
 
 books = []
+
 books.push Book.new(
   'Mr Greedy',
   changes: {
     Character['Mr Greedy'] => Attribute[:fatness].changes(from: :fat, to: :thin)
+  }
+)
+books.push Book.new(
+  'Little Miss Busy',
+  appearances: { Character['Mr Greedy'] => Attribute[:fatness].is(:thin) }
+)
+books.push Book.new(
+  'Mr Nosy',
+  changes: {
+    Character['Mr Nosy'] =>
+      Attribute[:nosiness].changes(from: :nosy, to: :not_nosy)
+  }
+)
+books.push Book.new(
+  'Little Miss Twins',
+  appearances: { Character['Mr Nosy'] => Attribute[:nosiness].is(:nosy) }
+)
+books.push Book.new(
+  'Mr Chatterbox',
+  changes: {
+    Character['Mr Chatterbox'] =>
+      Attribute[:chattiness].changes(from: :chatty, to: :quiet)
   }
 )
 
